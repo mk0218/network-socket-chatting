f92c7969108f5f32108fbb194c4f03e79c8e5b27
srlee056 <40015447+srlee056@users.noreply.github.com>
2019-03-30 19:00:33 +0900

    add voice chat

--- chat-client.py
+++ chat-client.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python3
 import os
-from socket import AF_INET, socket, SOCK_STREAM
+from socket import AF_INET, socket, SOCK_STREAM, SOCK_DGRAM
 from threading import Thread
+import pyaudio
 
 BUFSIZ = 1024
 
+# for voice 
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 44100
+RECORD_SECONDS = 40
+
+recv_host = "127.0.0.1"
+recv_port = 12346
+recv_frames = []
+send_frames = []
+
+send_host = "127.0.0.1"
+send_port = 12346
 
 def receive():
     while True:
@@ -34,10 +49,71 @@ def send():
 
     client_socket.close()
 
+# https://github.com/ami-GS/Network-Python/blob/master/Sound/soundTransferSer.py#L25
+def receive_voice_frame():
+    p_r = pyaudio.PyAudio()
+    recv_stream = p_r.open(format=FORMAT,
+						channels = CHANNELS,
+						rate = RATE,
+						output = True,
+						frames_per_buffer = CHUNK,
+						)
+    receive_udp_thread = Thread(target = receive_udp)
+    receive_udp_thread.setDaemon(True)
+    receive_udp_thread.start()
+    play_voice_thread = Thread(target = play_voice, args = (recv_stream))
+    play_voice_thread.setDaemon(True)
+    play_voice_thread.start()    
+    
+def receive_udp():
+    recv_udp_socket = socket(AF_INET, SOCK_DGRAM)
+    recv_udp_socket.bind((recv_host, recv_port))
+
+    while True:   
+        voice_data, recv_addr = recv_udp_socket.recvfrom(CHUNK*CHANNELS*2)
+        if voice_data:
+            recv_frames.append(voice_data)
+
+    recv_udp_socket.close()
+
+def play_voice(stream):
+    while True:
+        if len(recv_frames) == 10:
+            while True:
+                if len(recv_frames) > 0:
+                    stream.write(recv_frames.pop(0), CHUNK)
+
+
+def send_voice_frame():
+    p_s = pyaudio.PyAudio()
+    send_stream = p_s.open(format = FORMAT,
+						channels = CHANNELS,
+						rate = RATE,
+						input = True,
+						frames_per_buffer = CHUNK,
+						)
+    send_udp_thread = Thread(target = send_udp)
+    send_udp_thread.setDaemon(True)
+    send_udp_thread.start()
+    record_voice_thread = Thread(target = record_voice, args = (send_stream))
+    record_voice_thread.setDaemon(True)
+    record_voice_thread.start()
+
+def send_udp() :
+    send_udp_socket = socket(AF_INET, SOCK_DGRAM)
+    while True:
+        if len (send_frames) > 0 :
+            send_udp_socket.sendto(send_frames.pop(0), (send_host, send_port))
+    send_udp_socket.close()
+
+def record_voice(stream):
+    while True:
+        send_frames.append(stream.read(CHUNK))
 
 if __name__ == '__main__':  
     HOST = input('Enter host: ')
     PORT = input('Enter port: ')
+    PORT2 = 12346 # port for voice
     if not PORT:
         PORT = 12345
     else:
@@ -45,6 +121,8 @@ if __name__ == '__main__':
 
     ADDR = (HOST, PORT)
     client_socket = socket(AF_INET, SOCK_STREAM)
+
+    ADDR2 = (HOST, PORT2)
 
     try:
         client_socket.connect(ADDR)
@@ -55,3 +133,5 @@ if __name__ == '__main__':
         receive_thread.start()
         send_thread = Thread(target=send)
         send_thread.start()
+        
+        
