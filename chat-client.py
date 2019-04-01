4369a3affe52d474eac04b689dafb0fbb507fb7e
srlee056 <40015447+srlee056@users.noreply.github.com>
2019-04-02 08:50:30 +0900

    revised gui

--- chat-client.py
+++ chat-client.py
@@ -8,10 +8,10 @@ import tkinter
 BUFSIZ = 1024
 PORT = 12345
 # for voice 
-recv_host = '127.0.0.1'
+HOST = '127.0.0.1'
+RCV_HOST = '0.0.0.0'
 recv_port = 12347
 recv_frames = []
-send_host = '127.0.0.1'
 send_port = 12346
 send_frames = []
   
@@ -22,37 +22,15 @@ CHANNELS = 1
 RATE = 44100
 RECORD_SECONDS = 40
 
-onPressReturn = 1
+recv_msg = ''
+isReceived = 0
 
-def receive():
-    while True:
-        try:
-            msg = client_socket.recv(BUFSIZ).decode('utf-8')
-            if msg == '<quit>':
-                break
-            print(msg)
-        except OSError:
-            break
+send_msg = ''
+isSent = 0
 
 
-def send():
-    CLEAR_LINE = '\x1b[1A\x1b[2K'
-    while True:
-        try:
-            msg = input()
-            if msg == "<quit>":
-                client_socket.send(b'<quit>')
-                break
-            #if os.name != 'nt':
-            #    print(CLEAR_LINE, end='')
-            client_socket.send(msg.encode('utf-8'))
-
-        except OSError:
-            break
-    client_socket.close()
 
 
-    # https://github.com/ami-GS/Network-Python/blob/master/Sound/soundTransferSer.py#L25
 def receive_voice_frame():
     p_r = pyaudio.PyAudio()
     recv_stream = p_r.open(format=FORMAT,
@@ -69,13 +47,12 @@ def receive_voice_frame():
         
 def receive_udp():
     recv_udp_socket = socket(AF_INET, SOCK_DGRAM)
-    recv_udp_socket.bind((recv_host, recv_port))
+    recv_udp_socket.bind((RCV_HOST, recv_port))
 
     while True:   
         voice_data, recv_addr = recv_udp_socket.recvfrom(CHUNK*CHANNELS*2)
         if voice_data:
             recv_frames.append(voice_data)
-            print("receiving...")
 
     recv_udp_socket.close()
 
@@ -83,7 +60,6 @@ def play_voice(stream):
     while True:
         if len(recv_frames) >= 10 and len(recv_frames) > 0:
                     stream.write(recv_frames.pop(0), CHUNK)
-                    print("playing...")
 
 
 def send_voice_frame():
@@ -104,56 +80,70 @@ def send_udp() :
     send_udp_socket = socket(AF_INET, SOCK_DGRAM)
     while True:
         if len (send_frames) > 0 :
-            send_udp_socket.sendto(send_frames.pop(0), (send_host, send_port))
-            #print("sending...")
+            send_udp_socket.sendto(send_frames.pop(0), (HOST, send_port))
     send_udp_socket.close()
 
 def record_voice(stream):
     while True:
         send_frames.append(stream.read(CHUNK))
-        #print("recording...")
 
-def initGUI():
-    window=tkinter.Tk()
-    window.title("text/voice chat")
-    window.geometry("640x400+100+100")
-    window.resizable(False, False)
 
-    frame1 = tkinter.Frame()
-    frame1.pack()
-    label_var = tkinter.StringVar()
-    label_var.set("communicated texts")
-    label1 = tkinter.Label(frame1, textvariable = label_var)
-    label1.pack()
 
-    frame2 = tkinter.Frame()
-    frame2.pack()
-    text_log = tkinter.Text(frame2, width = 50, height = 25)
-    text_log.pack()
-
-    button_frame = tkinter.Frame()
-    button_frame.pack()
-
-    button_text = tkinter.StringVar()
-    entry = tkinter.Entry(button_frame, width = 40, textvariable = button_text)
-    entry.bind("<Return>", onPressReturn)
-    entry.pack()
-    exit_button = tkinter.Button(button_frame, text = "Exit") #, command=exitTrigger
-    exit_button.pack(side=tkinter.RIGHT, padx=100)
-    send_button = tkinter.Button(button_frame, text="Send") #, command=sendText
-    send_button.pack()
-		#self.button3 = tkinter.Button(frame3, text="Voice Recv On/Off", command=self.voiceRecvOnOff, bg='red')
-		#self.button3.pack()
-		#self.button4 = tkinter.Button(frame3, text="Voice Send On/Off", command=self.voiceSendOnOff, bg='red')
-		#self.button4.pack()
-        # 
+class Gui:
+    def __init__(self, Master):
+        self.master = Master
+        Master.title("text/voice chat")
+        Master.geometry("640x400+100+100")
+        Master.resizable(False, False)
     
+        self.masterFrame = tkinter.Frame(Master)
+        self.masterFrame.pack()
+
+        self.recv_frame = tkinter.Frame(self.masterFrame)
+        self.recv_frame.pack()
+        self.text_log = tkinter.Text(self.recv_frame, width = 60, height = 25)
+        self.text_log.pack()
+
+        self.button_frame = tkinter.Frame(self.masterFrame)
+        self.button_frame.pack()
+        self.text_to_send = tkinter.StringVar()
+        self.text_box = tkinter.Entry(self.button_frame, width = 40, textvariable = self.text_to_send)
+        self.text_box.bind("<Return>", self.pressSend)
+        self.text_box.pack()
+        self.send_button = tkinter.Button(self.button_frame, text="Send", command= self.send)
+        self.send_button.pack()
+
+    def pressSend(self, event):
+        self.send()
+
+    def receive(self):
+        while True:
+            try:
+                msg = client_socket.recv(BUFSIZ).decode('utf-8')
+                self.text_log.insert(tkinter.END, msg + '\n')
+                if msg == '<quit>':
+                    break   
+            except OSError:
+                break
+
+    def send(self):
+        msg = self.text_to_send.get()
+        print(msg)
+        if msg == "<quit>":
+            client_socket.send(b'<quit>')
+       # if msg == '':
+
+        client_socket.send(msg.encode('utf-8'))
+        self.text_log.insert(tkinter.END, msg + '\n')
+       # msg = ''
+       # client_socket.close()
+
 
 if __name__ == '__main__':  
-    initGUI()
+    window = tkinter.Tk()
+    app = Gui(window)
+
     HOST = input("Enter host: ")
-    
-    print("port")
 
     ADDR = (HOST, PORT)
     client_socket = socket(AF_INET, SOCK_STREAM)
@@ -167,8 +157,13 @@ if __name__ == '__main__':
     except ConnectionRefusedError:
         print("Server is not found.")
     else:
-        receive_thread = Thread(target=receive)
+        gui_thread = Thread(target = window.mainloop())
+        gui_thread.start()
+        receive_thread = Thread(target=app.receive())
         receive_thread.start()
-        send_thread = Thread(target=send)
-        send_thread.start()
-        
+        #send_thread = Thread(target=send)
+        #send_thread.start()
+
+    
+    
+
